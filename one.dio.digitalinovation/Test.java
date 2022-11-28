2827ef6a133bf5bfb63547dd924962b418d76bae
Vinicius Gomes <viniciusgomes05@hotmail.com>
2022-11-27 23:07:17 -0300

    Projeto Java puro

---
+++ one.dio.digitalinovation/Test.java
@@ -0,0 +1,33 @@
+package one.dio.digitalinovation;
+
+
+import one.dio.digitalinovation.SingletonEager;
+import one.dio.digitalinovation.SingletonLazy;
+import one.dio.digitalinovation.SingletonLazyHolder;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+	}
+
+}
